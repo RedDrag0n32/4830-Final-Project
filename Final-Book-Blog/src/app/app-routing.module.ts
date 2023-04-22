82933c6d8b15ad0a93007f5ebcc9f73e625946bd
Erika Zhou <ejzhou1222@gmail.com>
2023-04-21 19:02:47 -0500

    add routing stuff

--- Final-Book-Blog/src/app/app-routing.module.ts
+++ Final-Book-Blog/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BodyLibraryComponent } from "./body-library/body-library.component";
+import { BodyAddBookComponent } from "./body-add-book/body-add-book.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: BodyLibraryComponent
+  },
+  {
+    path:'add-book', component: BodyAddBookComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports:[RouterModule.forRoot(routes)],
+  exports:[RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+}
