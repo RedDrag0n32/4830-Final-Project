303c21e830b81e57b63d4f6f977cbb12e2db6eeb
Erika Zhou <ejzhou1222@gmail.com>
2023-04-26 21:15:46 -0500

    express set up
    Co-authored-by: Ty Kalscheur <RedDrag0n32@users.noreply.github.com>

--- Final-Book-Blog/backend/app.js
+++ Final-Book-Blog/backend/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use('/', (req, res, next) =>{
+app.get('/', (req, res, next) =>{
 res.send('Hello from express')
 
 })
