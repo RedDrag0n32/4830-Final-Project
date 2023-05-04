c05ee9098c424a033639850d796d2a3183131da4
RedDrag0n32 <ty.kalscheur@gmail.com>
2023-05-03 19:49:44 -0500

    Wish List component
    Set up the wish list component

--- Final-Book-Blog/frontend/src/app/body-wish-list/body-wish-list.component.ts
+++ Final-Book-Blog/frontend/src/app/body-wish-list/body-wish-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { BookServiceService } from '../book-service.service';
+import { Book } from '../book.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-body-wish-list',
@@ -8,5 +10,26 @@ import { BookServiceService } from '../book-service.service';
   styleUrls: ['./body-wish-list.component.css']
 })
 export class BodyWishListComponent {
+  public wishList: Book[] = []
+  private wishListSub: Subscription;
+  constructor(public service: BookServiceService){
 
+  }
+
+
+
+  ngOnInit(){
+    this.service.getLibrary();
+    this.wishListSub = this.service.getLibraryUpdateListener().subscribe((books: Book[]) =>{
+      books.forEach((element) => {
+        if(element.tag == "WishList") {
+          this.wishList.push(element)
+        }
+      })
+    })
+  }
+
+  ngOnDestroy() {
+    this.wishListSub.unsubscribe();
+  }
 }
